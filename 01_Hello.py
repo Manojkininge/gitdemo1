0017e391b1ffcea47b3a0b4a45f5a59a9d1297e0
manoj <kiningemanoj@gmail.com>
2022-09-06 22:43:37 +0530

    python program in GitStuff

---
+++ 01_Hello.py
@@ -0,0 +1,18 @@
+# single line comment
+
+'''multi line comment but it can also used for multiple line printing'''
+
+
+
+print("hellow world")
+
+
+print('''Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.''')
+
+from playsound import playsound
+ playsound ('D:\\Python Code with Harray\\happy-day.mp3')
